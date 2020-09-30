02665b10c1d96660e17a0c7adc3d120063b2abaf
RaymondBai <ruiwen.bai@gmail.com>
2020-09-30 02:20:26 -0500

    By utilizing sep feature of read.csv(), I was able to shorten the code and reduce run time fo execution dramatically.

--- run_analysis.R
+++ run_analysis.R
@@ -23,37 +23,21 @@ relevant_features <- gsub("(_){2,}", "_", relevant_features)
 relevant_features <- gsub("body_body_", "body_body", relevant_features)
 
 # Create an activity label chart (matching number and activity name)
-activity_labels <- read.csv("activity_labels.txt", header = FALSE)
-activity_labels <- activity_labels$V1
-activity_vec <- unlist(strsplit(activity_labels, split = " "))
+activity_labels <- read.csv("activity_labels.txt", sep = " ", header = FALSE)
+colnames(activity_labels) <- c("activity_id", "activity_name")
+activity_chart <- mutate(activity_labels, activity_name = tolower(activity_name))
 rm(activity_labels)
-activity_num <- numeric()
-activity_name <- character()
-for (i in 1:length(activity_vec)) {
-    if (i%%2 != 0){
-        activity_num <- append(activity_num, as.numeric(activity_vec[i]))
-    }
-    if (i%%2 == 0){
-        activity_name <- append(activity_name, activity_vec[i])
-    }
-}
-activity_chart <- data.frame(activity_num, tolower(activity_name))
-colnames(activity_chart) <- c("label", "activity")
-rm(i, activity_vec, activity_num, activity_name)
 
 # read in two portions of the full data
 setwd("~/Desktop/Coursera R/Getting:Cleaning Data/UCI HAR Dataset/test")
-test_subjects <- read.csv("subject_test.txt", header = FALSE)
-test_labels <- read.csv("y_test.txt", header = FALSE)
-test_set <- read.csv("X_test.txt", header = FALSE)
+test_subjects <- read.table("subject_test.txt")
+test_labels <- read.table("y_test.txt")
+test_set <- read.table("X_test.txt")
 
 setwd("~/Desktop/Coursera R/Getting:Cleaning Data/UCI HAR Dataset/train")
-training_subjects <- read.csv("subject_train.txt", header = FALSE)
-training_labels <- read.csv("y_train.txt", header = FALSE)
-training_set <- read.csv("X_train.txt", header = FALSE)
-
-# set working directory back to normal
-setwd("~/Desktop/Coursera R/Getting:Cleaning Data/CourseraDataCleaning")
+training_subjects <- read.table("subject_train.txt")
+training_labels <- read.table("y_train.txt")
+training_set <- read.table("X_train.txt")
 
 # combine testing and training data
 subject_ids <- rbind(test_subjects, training_subjects)
@@ -70,28 +54,17 @@ for (i in 1:nrow(activity_labels)) {
 }
 rm(i, activity_chart)
 
-# create data frame to contain recorded data in better format (90-100 s)
-df_data <- data.frame()
-for (i in 1:nrow(exp_data)) {
-    row_i <- exp_data[i,1]
-    row_i <- gsub(" {2}", " ", row_i)
-    data_i <- unlist(strsplit(row_i, " "))
-    if (data_i[1] == ""){
-        data_i <- data_i[!(data_i %in% "")]
-    }
-    data_i <- data_i[mean_or_std]
-    data_i <- gsub("-00", "-", data_i)
-    data_i <- gsub("\\+000", "+0", data_i)
-    data_i <- as.numeric(data_i)
-    df_data <- rbind(df_data, data_i)
-}
-rm(i, row_i, data_i, exp_data, mean_or_std)
+# Get only measured mean and standard deviation data
+df_data <- exp_data[,mean_or_std]
+rm(exp_data)
 
 # Combine three sets of data
 full_data <- cbind(subject_ids, activity_labels, df_data)
 colnames(full_data) <- c("subjectid", "activitylabel", relevant_features)
 full_data <- arrange(full_data, subject_ids)
 
+# set working directory back to normal
+setwd("~/Desktop/Coursera R/Getting:Cleaning Data/CourseraDataCleaning")
 
 # Calculate means
 full_data <- group_by(full_data, subjectid, activitylabel)
